cdce0e0e51167339523f2d72b15d78ae9da24c76
Krzysztof Knop <knop.krzysiek@gmail.com>
2024-01-09 15:18:46 +0100

    update pre-commit warnings

--- aifitness/users/tests/test_views.py
+++ aifitness/users/tests/test_views.py
@@ -12,11 +12,7 @@ from django.utils.translation import gettext_lazy as _
 from aifitness.users.forms import UserAdminChangeForm
 from aifitness.users.models import User
 from aifitness.users.tests.factories import UserFactory
-from aifitness.users.views import (
-    UserRedirectView,
-    UserUpdateView,
-    user_detail_view,
-)
+from aifitness.users.views import UserRedirectView, UserUpdateView, user_detail_view
 
 pytestmark = pytest.mark.django_db
 
--- aifitness/workouts/apps.py
+++ aifitness/workouts/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class WorkoutsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'workouts'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "workouts"

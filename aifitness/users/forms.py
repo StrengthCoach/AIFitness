8054b7078b4df88a1e6240e14dc93050f0049f1b
Krzysztof Knop <knop.krzysiek@gmail.com>
2024-01-18 20:10:18 +0100

    add forms for user proffile update form

--- aifitness/users/forms.py
+++ aifitness/users/forms.py
@@ -1,8 +1,10 @@
 from allauth.account.forms import SignupForm
 from allauth.socialaccount.forms import SignupForm as SocialSignupForm
+from bootstrap_datepicker_plus.widgets import DatePickerInput
 from django.contrib.auth import forms as admin_forms
 from django.contrib.auth import get_user_model
-from django.forms import EmailField
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.forms import CharField, ChoiceField, DateTimeField, DecimalField, EmailField, IntegerField, ModelForm
 from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
@@ -43,3 +45,48 @@ class UserSocialSignupForm(SocialSignupForm):
     Default fields will be added automatically.
     See UserSignupForm otherwise.
     """
+
+
+class UserProfileUpdateForm(ModelForm):
+    """
+    Form for updating user profile information.
+    """
+
+    first_name = CharField(max_length=255, label=_("First name of User"), required=True)
+    last_name = CharField(max_length=255, label=_("Last name of User"), required=True)
+    birth_date = DateTimeField(
+        widget=DatePickerInput(options={"format": "DD-MM-YYYY"}),
+        label=_("Date of birth"),
+        input_formats=["%d-%m-%Y"],
+        required=True,
+    )
+    gender = ChoiceField(choices=User.GENDER_CHOICES, label=_("Gender"), required=True)
+    height = IntegerField(label=_("Height"), required=True, validators=[MinValueValidator(1), MaxValueValidator(250)])
+    weight = DecimalField(
+        label=_("Weight"),
+        required=True,
+        validators=[MinValueValidator(1), MaxValueValidator(250)],
+        decimal_places=1,
+        max_digits=5,
+    )
+    level = ChoiceField(choices=User.LEVEL_CHOICES, label=_("Proficiency level"), required=True)
+    powerlifting_section = CharField(max_length=255, label=_("Powerlifting section"), required=False)
+    months_of_experience = IntegerField(
+        label=_("Number of months of experience"),
+        required=True,
+        validators=[MinValueValidator(0), MaxValueValidator(250)],
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "birth_date",
+            "gender",
+            "height",
+            "weight",
+            "level",
+            "powerlifting_section",
+            "months_of_experience",
+        ]
